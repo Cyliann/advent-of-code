bf08f774ac32b527c0fb6c58653dd24cbc4ba0f7
Cyliann <maksymilian@cych.eu>
2025-12-04 15:18:34 +0100

    feat: day 4 part 1

---
+++ 2025/day4_1.jl
@@ -0,0 +1,67 @@
+function build_map()
+  line_length = length(readline("data.txt"))
+  layout = Matrix{Bool}(undef, 0, line_length)
+
+  for line ∈ readlines("data.txt")
+    row = falses(line_length)
+    for (i, char) ∈ pairs(line)
+      row[i] = (char == '@')
+    end
+    layout = [layout; row']
+
+  end
+  layout
+end
+
+function is_idx_valid(idx::CartesianIndex, max_len::Int, max_height::Int)
+  if idx[1] < 1 || idx[2] < 1 || idx[1] > max_len || idx[2] > max_height
+    return false
+  end
+  true
+end
+
+function check_availability(pos, layout)
+  counter = 0
+  for i in range(-1, 1)
+    for j in range(-1, 1)
+      # skip yourself
+      if i == 0 && j == 0
+        continue
+      end
+
+      index = pos + CartesianIndex(i, j)
+
+      # checks if index is a valid index
+      if !is_idx_valid(index, size(layout)[1], size(layout)[2])
+        continue
+      end
+
+      if layout[index]
+        counter += 1
+      end
+    end
+  end
+  return counter < 4
+end
+
+function count_rolls(layout::Matrix)
+  counter = 0
+  for pos in CartesianIndices(layout)
+    if !layout[pos]
+      continue
+    end
+
+    if check_availability(pos, layout)
+      counter += 1
+    end
+  end
+  counter
+end
+
+function main()
+  layout = build_map()
+  roll_count = count_rolls(layout)
+  println(roll_count)
+end
+
+main()
